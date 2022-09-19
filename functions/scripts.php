4dd3ac28279ad90c959e9ba7c223ef3f9e5e823f
Ana Flávia Cador <aflaviah15@gmail.com>
2022-09-19 15:49:21 -0300

    rastrear pedido

--- functions/scripts.php
+++ functions/scripts.php
@@ -2,7 +2,7 @@
 function afc_load_styles() { 
     // variacoes declaradas
     $urltheme = get_template_directory_uri();
-    $vs = '7sEr4pd';
+    $vs = '7sEr8pd';
 
     ////////// css principal de layout
     wp_enqueue_style('layout', $urltheme . '/assets/css/layout.css?v='.$vs, array(), '', 'all', null); 
