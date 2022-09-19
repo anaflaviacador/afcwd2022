c48589f8a4e04b71ceaf0a4ff5d3999dd649178f
Ana Flávia Cador <aflaviah15@gmail.com>
2022-09-19 13:44:03 -0300

    Update intro-bt.php

--- parts/home/intro-bt.php
+++ parts/home/intro-bt.php
@@ -1,4 +1,4 @@
-<a href="/cotacao-projeto" class="botao grande inverso">
+<a href="/orcamento-projeto/" class="botao grande inverso">
     <?php esc_html_e( 'Faça sua cotação', 'afcwd2022' ); ?>
     <i class="fa-light fa-arrow-right-long bt-seta"></i>
 </a>
