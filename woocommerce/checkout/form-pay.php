d5035720027932cd014038e11b462367df732d19
Ana Flávia Cador <aflaviah15@gmail.com>
2022-09-27 12:36:57 -0300

    Update form-pay.php

--- woocommerce/checkout/form-pay.php
+++ woocommerce/checkout/form-pay.php
@@ -19,7 +19,7 @@ defined( 'ABSPATH' ) || exit;
 
 $totals = $order->get_order_item_totals(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 ?>
-<div class="container-medio">
+<div class="container-medio pb-4em pt-2em">
 <form id="order_review" method="post">
 
 	<table class="shop_table">
@@ -89,7 +89,7 @@ $totals = $order->get_order_item_totals(); // phpcs:ignore WordPress.WP.GlobalVa
 
 			<?php do_action( 'woocommerce_pay_order_before_submit' ); ?>
 
-			<?php echo apply_filters( 'woocommerce_pay_order_button_html', '<button type="submit" class="botao afirmacao grande fullwidth" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button>' ); // @codingStandardsIgnoreLine ?>
+			<?php echo apply_filters( 'woocommerce_pay_order_button_html', '<button type="submit" class="botao afirmacao grande fullwidth mt-1em" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button>' ); // @codingStandardsIgnoreLine ?>
 
 			<?php do_action( 'woocommerce_pay_order_after_submit' ); ?>
 
