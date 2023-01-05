41ec8c579d24f7d9e1498782a86e8c6786a646f9
Ana Flávia Cador <aflaviah15@gmail.com>
2023-01-05 15:01:46 -0300

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -4,6 +4,6 @@
 <?php get_template_part('parts/home/servicos'); ?>
 <?php get_template_part('parts/projetos-destaque'); ?>
 <?php get_template_part('parts/home/ana'); ?>
-<?php afc_depoimentos(7,''); ?>
+<?php afc_depoimentos(7,'projeto'); ?>
 
 <?php get_footer(); ?>
