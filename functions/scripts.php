2753e9f038d946d812ca41d1981cdc53952c19bd
Ana Flávia Cador <aflaviah15@gmail.com>
2023-06-25 12:27:03 -0300

    Update scripts.php

--- functions/scripts.php
+++ functions/scripts.php
@@ -2,7 +2,7 @@
 function afc_load_styles() { 
     // variacoes declaradas
     $urltheme = get_template_directory_uri();
-    $vs = 'fjE4B35';
+    $vs = '5Fyk4Qf';
 
     ////////// css principal de layout
     wp_enqueue_style('layout', $urltheme . '/assets/css/layout.css?v='.$vs, array(), '', 'all', null); 
