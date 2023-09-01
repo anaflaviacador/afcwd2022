5ef82d2f7d4d05b2e5713657c667757a3a1f8f5a
Ana Flávia Cador <aflaviah15@gmail.com>
2023-09-01 10:06:39 -0300

    Update forma-trabalho.php

--- parts/proposta/forma-trabalho.php
+++ parts/proposta/forma-trabalho.php
@@ -19,12 +19,14 @@ $img = '6441'; // producao
                 <div data-w-id="6fd14137-c52c-2d99-1152-dfd460833302" class="timeline-full"></div>
             </div>
 
-            <?php if ( have_rows( 'proposta_passos' ) ) : ?>
-                <?php while ( have_rows( 'proposta_passos' ) ) : the_row(); ?>
+            <?php if ( have_rows( 'proposta_passos' ) ) : $i=0; ?>
+                <?php $count = count(get_field('proposta_passos')); ?>
+                <?php while ( have_rows( 'proposta_passos' ) ) : $i++; the_row(); ?>
                     <?php $titulo = get_sub_field( 'titulo' ); ?>
                     <?php $plataforma = get_sub_field( 'plataforma' ); ?>
                     <?php $descricao = get_sub_field( 'descricao' ); ?>
-                    <div class="w-layout-vflex prop-timeline-box">
+
+                    <div class="w-layout-vflex prop-timeline-box<?php echo $count == $i ? ' timeline-end': ''; ?>">
                         <div class="timeline-dot"></div>
                         
                         <h2><?php echo wp_strip_all_tags( $titulo ); ?></h2>
