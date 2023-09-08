f98b4095a7b081d740dc638122ddbee53d5b94c4
Ana Flávia Cador <aflaviah15@gmail.com>
2023-09-08 12:35:35 -0300

    Update valores.php

--- parts/proposta/valores.php
+++ parts/proposta/valores.php
@@ -40,8 +40,8 @@
                     <em>Urgência na entrega? O investimento é de <?php echo wp_strip_all_tags($urgencia['valor_mensal']); ?> ou <?php echo wp_strip_all_tags($urgencia['valor_vista']); ?> à vista</em><br>
                     <?php endif; endif; ?>
 
-                    <i class="fa-regular fa-clock cor-azul-medio"></i> Prazo padrão: <?php echo esc_attr($valor['prazo']); ?> dias <span class="sublinhado">úteis</span> &nbsp;|&nbsp; 
-                    <?php if($urgencia) : if($urgencia['prazo']) : ?> Prazo na urgência: <?php echo esc_attr($urgencia['prazo']); ?> dias <span class="sublinhado">corridos</span>.<?php endif; endif; ?>
+                    <i class="fa-regular fa-clock cor-azul-medio"></i> Prazo padrão: <?php echo esc_attr($valor['prazo']); ?> dias <span class="sublinhado">úteis</span>
+                    <?php if($urgencia) : if($urgencia['prazo']) : ?> &nbsp;|&nbsp;  Prazo na urgência: <?php echo esc_attr($urgencia['prazo']); ?> dias <span class="sublinhado">corridos</span>.<?php endif; endif; ?>
                 </p>
                 
             <?php endif; endif; ?>
