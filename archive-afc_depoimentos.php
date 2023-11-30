2842286085ac61347652fa93d138a62d51e8f681
Ana Flávia Cador <aflaviah15@gmail.com>
2023-11-30 14:34:18 -0300

    Update archive-afc_depoimentos.php

--- archive-afc_depoimentos.php
+++ archive-afc_depoimentos.php
@@ -1,8 +1,32 @@
-<?php get_header(); ?>
+
+<?php 
+
+$args = array(
+	'post_type' => 'afc_depoimentos', 
+	'order' => 'DESC', 
+	'orderby' => 'rand',
+	'posts_per_page' => -1,
+);
+	
+$depo = new WP_Query($args);
+
+
+get_header(); ?>
 
 <div class="container pb-4em">
 
-	<?php echo do_shortcode('[ajax_load_more post_type="afc_depoimentos" theme_repeater="depoimento.php" container_type="div" posts_per_page="8" images_loaded="true" scroll="true" scroll_distance="-150" transition_container_classes="clientes-lista" transition="masonry" masonry_animation="slide-up" masonry_selector=".cliente" button_label="<span>'.esc_html__( 'Mais relatos', 'afcwd2022' ).'</span>" button_loading_label="<span>'.esc_html__( 'Carregando', 'afcwd2022' ).'</span>" button_done_label="<span>'.esc_html__( 'Isso é tudo!', 'afcwd2022' ).'</span>" ]'); ?>
+
+	<?php if ( $depo->have_posts() ) : ?>
+		<div class="clientes-lista">    
+			<?php while ( $depo->have_posts() ) : $depo->the_post(); ?>
+
+				<?php get_template_part('alm_templates/depoimento'); ?>
+
+			<?php endwhile; ?>
+		</div>
+	<?php endif; ?>
+	
+	<?php // do_shortcode('[ajax_load_more post_type="afc_depoimentos" theme_repeater="depoimento.php" container_type="div" posts_per_page="8" images_loaded="true" scroll="true" scroll_distance="-150" transition_container_classes="clientes-lista" transition="masonry" masonry_animation="slide-up" masonry_selector=".cliente" button_label="<span>'.esc_html__( 'Mais relatos', 'afcwd2022' ).'</span>" button_loading_label="<span>'.esc_html__( 'Carregando', 'afcwd2022' ).'</span>" button_done_label="<span>'.esc_html__( 'Isso é tudo!', 'afcwd2022' ).'</span>" ]'); ?>
 
     <div class="has-text-align-center mt-2em">
       <div class="mb-10px">
