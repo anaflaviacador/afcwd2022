c2074fee2350b6602e2b37c225727465242b7b59
Ana Flávia Cador <aflaviah15@gmail.com>
2025-05-12 09:11:24 -0300

    Update archive-etheme_portfolio.php

--- archive-etheme_portfolio.php
+++ archive-etheme_portfolio.php
@@ -3,7 +3,7 @@ $urlHome = esc_url(home_url('/'));
 $urlTema = get_template_directory_uri();
 ?>
 
-<?php $args = array( 'post_type' => 'etheme_portfolio', 'order' => 'DESC', 'posts_per_page' => 2 );?>
+<?php $args = array( 'post_type' => 'etheme_portfolio', 'order' => 'DESC', 'posts_per_page' => -1 );?>
 <?php $trabalhos = new WP_Query($args); ?>
 
 <div class="lista-projetos">
@@ -21,7 +21,7 @@ $urlTema = get_template_directory_uri();
 <?php endwhile; endif; wp_reset_query(); ?>
 </div>
 
-<?php echo do_shortcode('[ajax_load_more post_type="etheme_portfolio" theme_repeater="projeto.php" container_type="div" posts_per_page="4" offset="2" images_loaded="true" scroll="false" transition_container_classes="lista-projetos" button_label="<span>'.esc_html__( 'Mais projetos', 'afcwd2022' ).'</span>" button_loading_label="<span>'.esc_html__( 'Carregando', 'afcwd2022' ).'</span>" button_done_label="<span>'.esc_html__( 'Isso é tudo!', 'afcwd2022' ).'</span>" ]'); ?>
+<?php echo //do_shortcode('[ajax_load_more post_type="etheme_portfolio" theme_repeater="projeto.php" container_type="div" posts_per_page="4" offset="2" images_loaded="true" scroll="false" transition_container_classes="lista-projetos" button_label="<span>'.esc_html__( 'Mais projetos', 'afcwd2022' ).'</span>" button_loading_label="<span>'.esc_html__( 'Carregando', 'afcwd2022' ).'</span>" button_done_label="<span>'.esc_html__( 'Isso é tudo!', 'afcwd2022' ).'</span>" ]'); ?>
 
 
 
